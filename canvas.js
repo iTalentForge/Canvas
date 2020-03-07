00bd122c47455e39ee68dbee933aab4faf8a94f8
alexey <tozikalexey@gmail.com>
2020-03-07 09:41:34 +0300

    Add all methods

--- canvas.js
+++ canvas.js
@@ -2,40 +2,169 @@ class CreateCanvas {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.canvas = [];
+        this.getCanvas();
     }
 
-    canvas() {
-        if (this.width < 0) return "Width must be more than 0";
-        if (this.height < 0) return "Height must be more than 0";
+    getCanvas() {
+        if (this.width < 0) console.log("Width must be more than 0");
+        if (this.height < 0) console.log("Height must be more than 0");
 
-        let canvas = [];
+        for (let x = 0; x < this.height + 2; x++) {
 
-        for (let x = 0; x < this.width + 2; x++) {
+            this.canvas.push([]);
 
-            canvas.push([]);
+            for (let y = 0; y < this.width + 2; y++) {
+                this.canvas[x][y] = ' ';
 
-            for (let y = 0; y < this.height + 2; y++) {
-                canvas[x][y] = 0;
-                
-                if(x == 0 || x == this.width + 1) {
-                    canvas[x][y] = '-';
+                if (x == 0 || x == this.height + 1) {
+                    this.canvas[x][y] = '-';
                 }
-                
-                if (y == 0 || y == this.height + 1) {
-                    canvas[x][y] = '|';
+
+                if (y == 0 || y == this.width + 1) {
+                    this.canvas[x][y] = '|';
                 }
             }
         }
-
-
-        console.log(canvas);
 
     }
 
     drawCanvas(x1, y1, x2, y2) {
 
+        this.validateInput(x1, y1, x2, y2);
+
+        if (x1 != x2) {
+            for (let i = x1; i <= x2; i++) {
+                this.canvas[y1][i] = 'x';
+            }
+        } else {
+            for (let i = y1; i <= y2; i++) {
+                this.canvas[i][x1] = 'x';
+            }
+        }
+        
     }
+
+    drawRectangle(x1, y1, x2, y2) {
+
+        this.validateInput(x1, y1, x2, y2);
+
+        for (let i = x1; i <= x2; i++) {
+            this.canvas[y1][i] = 'x';
+            this.canvas[y2][i] = 'x';
+        }
+
+        for (let i = y1; i <= y2; i++) {
+            this.canvas[i][x1] = 'x';
+            this.canvas[i][x2] = 'x';
+        }
+    }
+
+    bucketFill(x, y, color) {
+        let curentColor;
+        if (this.height < y || this.width < x || x < 0 || y < 0) {
+            console.log("Can't fill out of board");
+        } else if (this.canvas[y][x] == "x") {
+            console.log("Can't fill the line");
+        } else if (this.canvas[y][x] == color) {
+            return;
+        }
+
+        curentColor = this.canvas[y][x];
+        
+        let [leftCorner, rightCorner] = this.findBorders(x, y);
+
+        for (let i = leftCorner + 1; i < rightCorner; i++) {
+            this.canvas[y][i] = color;
+            if (this.canvas[y + 1][i] == curentColor) {
+                this.bucketFill(i, y + 1, color)
+            }
+            if (this.canvas[y - 1][i] == curentColor) {
+                this.bucketFill(i, y - 1, color)
+            }
+        }
+
+    }
+
+    findBorders(x, y) {
+        let leftCorner = +x;
+        let rightCorner = +leftCorner;
+
+        while (this.canvas[y][leftCorner] != 'x') {
+            if (leftCorner <= 0) break;
+            leftCorner -= 1;
+        }
+
+        while (this.canvas[y][rightCorner] != 'x') {
+            if (rightCorner > this.width) break;
+            rightCorner += 1;
+        }
+
+        return [leftCorner, rightCorner];
+    }
+
+    validateInput(x1, x2, y1, y2) {
+        if (x1 <= 0 || x2 <= 0 || y1 <= 0 || y2 <= 0) {
+            console.log("Can't draw on borders");
+        }
+    }
+
+    parser() {
+        let strCanvas = '';
+
+        for (let line = 0; line < this.canvas.length; line++) {
+            for (let charNum = 0; charNum < this.canvas[line].length; charNum++) {
+                if (charNum == this.canvas[line].length - 1) {
+                    let val = this.canvas[line][charNum] + '\n';
+                    strCanvas += val;
+                } else {
+                    strCanvas += this.canvas[line][charNum];
+                }
+            }
+        }
+
+        document.getElementById("main").innerHTML = strCanvas;
+
+    }
+
+    outputFile(input) {
+        let file = input.files[0];
+
+        let reader = new FileReader();
+
+        reader.readAsText(file);
+
+        reader.onload = () => {
+            let commandList = reader.result.split('\n');
+
+            for(let i = 0; i < commandList.length; i++) {
+                let command = commandList[i].split(' ');
+
+                if (command[0] == 'C') {
+                    console.log('hi');
+                } else if (command[0] == 'L') {
+                    this.drawCanvas(+command[1], +command[2], +command[3], +command[4])
+                } else if (command[0] == 'R') {
+                    this.drawRectangle(+command[1], +command[2], +command[3], +command[4])
+                } else if (command[0] == 'B') {
+                    this.bucketFill(+command[1], +command[2], command[3])
+                } else {
+                    return;
+                }
+            }
+        };
+
+        reader.onerror = () => {
+            console.log(reader.error);
+        };
+
+    }
+
+
 }
 
-let lolkek = new CreateCanvas(10, 10);
-lolkek.canvas();
+let lolkek = new CreateCanvas(20, 4);
+// lolkek.drawCanvas(1, 2, 6, 2);
+// lolkek.drawCanvas(6, 3, 6, 4);
+// lolkek.drawRectangle(16, 1, 20, 3);
+// lolkek.bucketFill(5, 3, 'o');
